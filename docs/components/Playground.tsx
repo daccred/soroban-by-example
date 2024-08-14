26b11583e5c245ebbd0d53191fb18d6f08ea3790
Mercy Thaddeus <mercythaddeus234+github@gmail.com>
2024-08-14 20:55:01 +0100

    add okashi playground

---
+++ docs/components/Playground.tsx
@@ -0,0 +1,10 @@
+
+
+export const Playground = () => {
+  return (
+    <div>
+        <iframe className="mt-6" width="100%" height="600px" frameBorder="0" src="https://okashi-proxy-production.up.railway.app/playground/bllohuzgicdvqlmvfqwzexhdafwq">
+        </iframe>
+    </div>
+  )
+}
