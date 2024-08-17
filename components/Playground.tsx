90456c9651ea9431074482a71100cb98a9652eab
Koha <omobolathejoshua@gmail.com>
2024-08-17 18:19:36 +0100

    feat: add run on okashi button

--- components/Playground.tsx
+++ components/Playground.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { OkashiSvg, SorobanSvg } from './Icons';
 
 type PlaygroundProps = {
   okashiID: string
@@ -28,6 +29,26 @@ const loadingTextStyle = {
   color: '#4B5563'
 }
 
+const buttonStyle = {
+  position: 'absolute' as const,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '0.5rem',
+  top: '0',
+  right: '0',
+  backgroundColor: "#312f36",
+  padding: '0.5rem 1.3rem',
+  cursor: 'pointer',
+  fontSize: '1rem',
+  fontWeight: 600,
+  color: '#fff',
+  transition: 'background 0.2s ease-in-out',
+  '&:hover': {
+    backgroundColor: 'rgba(0,0,0,0)'
+  }
+}
+
 
 export const Playground: React.FC<PlaygroundProps> = ({ okashiID }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -40,6 +61,17 @@ export const Playground: React.FC<PlaygroundProps> = ({ okashiID }) => {
           <p style={loadingTextStyle}>Loading playground...</p>
         </div>
       )}
+
+      <div>
+        <a href={`https://okashi.dev/playground/${okashiID}`} target='_blank' style={buttonStyle}>
+          <span>
+            <OkashiSvg style={{width: '20px', height: '24px', color: '#fff' }}/>
+          </span>
+          <span>
+            Run on Okashi
+          </span>
+        </a>
+      </div>
 
       <iframe
         width="100%" height="600px"
