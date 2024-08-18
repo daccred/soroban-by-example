8a0cf039f88c4c0915388fd6aea0c666353121c4
000 lancelot <jupilance@gmail.com>
2024-08-18 17:26:07 +0000

    custom typography as base64 in root style.css

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,47 +1,57 @@
-import { defineConfig } from 'vocs'
-import sidebar from './sidebar'
+import { defineConfig } from "vocs";
+import sidebar from "./sidebar";
 
 export default defineConfig({
-  title: 'Soroban-by-example',
-  description: 'Learn soroban by studing simple snipperts',
-  titleTemplate: '%s - Soroban-by-example',
+  title: "Soroban by Example",
+  description: "Learn soroban by exploring code samples",
+  titleTemplate: "%s - soroban-by-example",
+  /**@see https://vocs.dev/docs/api/config#element */
+  // head: (
+  //    <>
+  //      <meta property="og:type" content="website" />
+  //      <meta property="og:title" content="viem · TypeScript Interface for Ethereum" />
+  //      <meta property="og:image" content="https://viem.sh/og-image.png" />
+  //      <meta property="og:url" content="https://viem.sh" />
+  //      <meta property="og:description" content="Build reliable Ethereum apps & libraries with lightweight, composable, & type-safe modules from viem." />
+  //    </>
+  //  ),
   sidebar,
-  rootDir: '.',
-  iconUrl: {light: '/sbe-icon-light.svg', dark: '/sbe-icon-dark.svg'},
-  logoUrl: {light: '/sbe-icon-light.svg', dark: '/sbe-icon-dark.svg'},
+  rootDir: ".",
+  iconUrl: { light: "/sbe-icon-light.svg", dark: "/sbe-icon-dark.svg" },
+  logoUrl: { light: "/sbe-icon-light.svg", dark: "/sbe-icon-dark.svg" },
   socials: [
     {
-      icon: 'github',
-      link: 'https://github.com/kohasummons/soroban-by-example',
+      icon: "github",
+      link: "https://github.com/kohasummons/soroban-by-example",
     },
     {
-      icon: 'discord',
-      link: 'https://discord.gg/kohasummons',
+      icon: "discord",
+      link: "https://discord.gg/kohasummons",
     },
     {
-      icon: 'x',
-      link: 'https://x.com/stellar',
+      icon: "x",
+      link: "https://x.com/stellar",
     },
   ],
   theme: {
     accentColor: {
-      light: '#ff9318',
-      dark: '#ffc517',
+      light: "#ff9318",
+      dark: "#ffc517",
     },
   },
   topNav: [
     {
-      text: 'Playground',
+      text: "Playground",
       items: [
         {
-          text: 'okashi',
-          link: '/introduction',
+          text: "okashi",
+          link: "/introduction",
         },
       ],
     },
     {
-      text: 'Code Snippet',
-      link: 'https://github.com/kohasummons',
+      text: "Code Snippet",
+      link: "https://github.com/kohasummons",
     },
     {
       text: "Soroban",
@@ -51,25 +61,25 @@ export default defineConfig({
           link: `/introduction`,
         },
         {
-          text: 'Changelog',
-          link: 'https://github.com/kohasummons/soroban-by-example',
+          text: "Changelog",
+          link: "https://github.com/kohasummons/soroban-by-example",
         },
         {
-          text: 'Contributing',
-          link: 'https://github.com/kohasummons/soroban-by-example/blob/main/.github/CONTRIBUTING.md',
+          text: "Contributing",
+          link: "https://github.com/kohasummons/soroban-by-example/blob/main/.github/CONTRIBUTING.md",
         },
       ],
     },
   ],
   search: {
     boostDocument(documentId) {
-      if (documentId.startsWith('pages/docs')) return 2
-      return 1
+      if (documentId.startsWith("pages")) return 2;
+      return 1;
     },
   },
   editLink: {
-    pattern: 'https://github.com/kohasummons/soroban-by-example/edit/main/pages/:path',
-    text: 'Suggest changes to this page',
+    pattern:
+      "https://github.com/kohasummons/soroban-by-example/edit/main/pages/:path",
+    text: "Suggest changes to this page",
   },
-  
-})
+});
