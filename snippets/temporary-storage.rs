fab3a9124e46f9b1797a6cca96eced3312f344e9
Usheninte Dangana <ninte@tuta.io>
2024-09-25 11:16:45 +0100

    Completed temporary storage snippet and doc page

---
+++ snippets/temporary-storage.rs
@@ -0,0 +1,28 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl, contracttype, Env};
+
+#[contracttype]
+pub enum DataKey {
+    MyKey
+}
+
+#[contract]
+pub struct TemporaryStorage;
+
+#[contractimpl]
+impl TemporaryStorage {
+    /// Create contract entry
+
+    pub fn setup(env: Env) {
+        env.storage().temporary().set(&DataKey::MyKey, &0);
+    }
+
+    /// Extend the TTL (Time-To-Live) to 7000 ledgers
+    /// in the case that TTL is less than 3000 ledgers
+
+    pub fn extend_persistent(env: Env) {
+        env.storage()
+            .temporary()
+            .extend_ttl(&DataKey::MyKey, 3000, 7000);
+    }
+}
