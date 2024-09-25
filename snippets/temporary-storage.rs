864fc2c46bf3b600b6afd53df88783a5e9b8e9a9
Usheninte Dangana <ninte@tuta.io>
2024-09-25 11:19:06 +0100

    Corrected function name in temporary storage file

--- snippets/temporary-storage.rs
+++ snippets/temporary-storage.rs
@@ -20,7 +20,7 @@ impl TemporaryStorage {
     /// Extend the TTL (Time-To-Live) to 7000 ledgers
     /// in the case that TTL is less than 3000 ledgers
 
-    pub fn extend_persistent(env: Env) {
+    pub fn extend_temporary(env: Env) {
         env.storage()
             .temporary()
             .extend_ttl(&DataKey::MyKey, 3000, 7000);
